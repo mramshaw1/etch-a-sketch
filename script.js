adb219237ad7b081cdf82a2d825d656a6e35f13c
mramshaw1 <m.ramshaw@hotmail.co.uk>
2023-04-11 12:07:58 +0100

    Buttons added to html to clear and resize the grid, currently only the clear grid button works

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
+const container = document.querySelector('.container');
+const resizeGrid = document.querySelector('.resize-grid');
+let clearGrid = document.querySelector('.clear-grid');
 
-function genDivs(x,j){
-    const container = document.querySelector('.container');
-    
-    
-    for(let i = 0; i<x; i++){
-        const row = document.createElement('div');
+function genDivs(value) {
+
+
+
+    for (let i = 0; i < value; i++) {
+        let row = document.createElement('div');
         row.classList.add('theRows');
 
-    for(let y = 0; y<j; y++){
-        const column = document.createElement('div');
-        column.classList.add('theColumns');
-        row.appendChild(column)
-        column.addEventListener('mouseover',(event) => {
-        event.target.style.background = "black";
-});
+        for (let y = 0; y < value; y++) {
+            let column = document.createElement('div');
+            column.classList.add('theColumns');
+            row.appendChild(column)        
+            container.appendChild(row);
+
+        }
+        let column = document.querySelectorAll('.theColumns');
+        column.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = "black";
+        }));
+
+    }
+    resetGrid = () => {
+        let column = document.querySelectorAll('.theColumns');
+        column.forEach(box => {
+            box.style.backgroundColor = "white";
+            console.log("clear")
+        })
 }
-                    container.appendChild(row);
-
+clearGrid.addEventListener('click', resetGrid);
+resizeGrid.addEventListener('click', function(){
+    let value = prompt("please eneter the size of grid you would like");
+    if (typeof value !== 'number'){
+        return
+    }
+    genDivs(value)
+})
 }
 
 
-}
+genDivs(10)
 
 
 
 
 
-genDivs(16,16)
 
-
-    
 
