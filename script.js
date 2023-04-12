5597ae3786cb16a39c77050dbb0bdb0abd726707
mramshaw1 <m.ramshaw@hotmail.co.uk>
2023-04-12 10:24:29 +0100

    An event listener has been added to allow the user to select the size of grid they would like, currently working on adding an event listener to the reset button to clear all the black squares

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let container = document.querySelector('.container');
 const resetbtn = document.querySelector('.reset-button');
 const gridSize = document.querySelector('.grid-size');
 
+generateGrid(16)
+
 function generateGrid(size){
     for(let i = 1; i<=size; i++){
     const row = document.createElement('div');
@@ -12,12 +14,34 @@ function generateGrid(size){
         const column = document.createElement('div');
         column.classList.add('column');
         row.appendChild(column)
+        
     }
     }
-
+draw();
 
 }
+function draw(){
+    let columns = document.getElementsByClassName('column');
+    for (let i =0; i<columns.length; i++){
+    columns[i].addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "black"
+}) }
+}
 
-generateGrid(16)
+gridSize.addEventListener('click', function (){
+    const value = prompt("please choose a number for your grid size")
+    container.textContent = ''
+    generateGrid(value)
+})
+
+resetbtn.addEventListener('click', function() {
+    let columns = document.querySelector('column');
+    for(let i = 0; i<columns.length; i++){
+        columns.style.backgroundColor = "white"
+    }
+})
+
+
+
 
 
