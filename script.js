37528fd5b5f925c371072c417663e4dff742d3d8
mramshaw1 <m.ramshaw@hotmail.co.uk>
2023-04-12 09:29:03 +0100

    Finally have a working flexbox grid. With the use of flex 1 and nesting a for loop.

--- script.js
+++ script.js
@@ -1,51 +1,23 @@
-const container = document.querySelector('.container');
-const resizeGrid = document.querySelector('.resize-grid');
-let clearGrid = document.querySelector('.clear-grid');
 
-function genDivs(value) {
+let container = document.querySelector('.container');
+const resetbtn = document.querySelector('.reset-button');
+const gridSize = document.querySelector('.grid-size');
 
-
-
-    for (let i = 0; i < value; i++) {
-        let row = document.createElement('div');
-        row.classList.add('theRows');
-
-        for (let y = 0; y < value; y++) {
-            let column = document.createElement('div');
-            column.classList.add('theColumns');
-            row.appendChild(column)        
-            container.appendChild(row);
-
-        }
-        let column = document.querySelectorAll('.theColumns');
-        column.forEach(box => box.addEventListener('mouseover', () => {
-            box.style.background = "black";
-        }));
-
+function generateGrid(size){
+    for(let i = 1; i<=size; i++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for(let i = 1; i<= size; i++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column)
     }
-    resetGrid = () => {
-        let column = document.querySelectorAll('.theColumns');
-        column.forEach(box => {
-            box.style.backgroundColor = "white";
-            console.log("clear")
-        })
-}
-clearGrid.addEventListener('click', resetGrid);
-resizeGrid.addEventListener('click', function(){
-    let value = prompt("please eneter the size of grid you would like");
-    if (typeof value !== 'number'){
-        return
     }
-    genDivs(value)
-})
+
+
 }
 
-
-genDivs(10)
-
-
-
-
-
+generateGrid(16)
 
 
